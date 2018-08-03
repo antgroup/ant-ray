0d38bf71695ff0c85756a7f37aa2780dc7b3236e
kingchin1218 <kingchin1218@126.com>
2018-08-03 10:16:46 +0800

    test

--- java/test_cluster.sh
+++ java/test_cluster.sh
@@ -15,7 +15,8 @@ function run_test() {
     rm -rf local_deploy
     ./prepare.sh -t local_deploy
     pushd local_deploy
-    local_ip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
+    #local_ip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
+    local_ip=`ifconfig |grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
     echo "use local_ip" $local_ip
 
     OVERWRITE="ray.java.start.redis_port=34222;ray.java.start.node_ip_address=$local_ip;ray.java.start.deploy=true;ray.java.start.run_mode=CLUSTER;ray.java.start.raylet_port=35567;"

5d7d960e736c5922547d8e89d2bf1ee0e2a0d9d0
Yuhong Guo <yuhong.gyh@antfin.com>
2018-08-04 22:38:53 +0800

    Change the training task in CI to xray

--- test/jenkins_tests/multi_node_tests/test_rllib_eval.sh
+++ test/jenkins_tests/multi_node_tests/test_rllib_eval.sh
@@ -7,7 +7,7 @@ GYM_ENV='CartPole-v0'
 for ALG in $ALGS
 do
   EXPERIMENT_NAME=$GYM_ENV'_'$ALG
-  python /ray/python/ray/rllib/train.py --run $ALG --env $GYM_ENV \
+  RAY_USE_XRAY=1 python /ray/python/ray/rllib/train.py --run $ALG --env $GYM_ENV \
     --stop '{"training_iteration": 2}' --experiment-name $EXPERIMENT_NAME \
     --checkpoint-freq 1
 
@@ -15,7 +15,7 @@ do
   CHECKPOINT_FOLDER=$(ls $EXPERIMENT_PATH)
   CHECKPOINT=$EXPERIMENT_PATH'/'$CHECKPOINT_FOLDER'/checkpoint-1'
 
-  python /ray/python/ray/rllib/eval.py $CHECKPOINT --run $ALG \
+  RAY_USE_XRAY=1 python /ray/python/ray/rllib/eval.py $CHECKPOINT --run $ALG \
     --env $GYM_ENV --no-render
 
   # Clean up

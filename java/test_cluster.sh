df4d5812ede20f8f5fe5c54ff1554c6a2fb5cbff
kingchin1218 <kingchin1218@126.com>
2018-08-18 20:04:19 +0800

    Add an example for test_cluster.sh

--- java/test_cluster.sh
+++ java/test_cluster.sh
@@ -37,7 +37,7 @@ function run_test() {
     fi
     popd
 
-    cp -rf tutorial/target/ray-tutorial-1.0.jar example/app1/
+    cp -rf test/target/ray-test-1.0.jar example/app1/
     pushd example
     zip -r app1.zip app1
     popd
@@ -45,7 +45,7 @@ function run_test() {
     # run with cluster mode
     pushd local_deploy
     export RAY_CONFIG=ray/ray.config.ini
-    ARGS=" --package ../example/app1.zip --class org.ray.exercise.Exercise02 --args=test1,test2  --redis-address=$local_ip:34222"
+    ARGS=" --package ../example/app1.zip --class org.ray.test.example.HelloExample --args=test1,test2  --redis-address=$local_ip:34222"
     ../local_deploy/run.sh submit $ARGS
     popd
 
@@ -63,7 +63,7 @@ function run_test() {
     [[ ${start_process_log} =~ "Started Ray head node" ]] || exit 1
     echo "Check: Ray all processes started."
 
-    execution_log=$(cat "./local_deploy/ray/run/org.ray.exercise.Exercise02/0.out.txt")
+    execution_log=$(cat "./local_deploy/ray/run/org.ray.test.example.HelloExample/0.out.txt")
     [[ ${execution_log} =~ "hello,world!" ]] || exit 1
     echo "Check: The tests ran successfully."
 }

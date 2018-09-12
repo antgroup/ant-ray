1aa7bbcdd52eb4269967cfacd2070fe46cb28e49
kingchin1218 <kingchin1218@126.com>
2018-09-12 20:57:49 +0800

    [Refactor] Clean some code.
    [Refactor] Improve Runtime.
    
    [Refactor] Use Java properties to configure rayparameters.
    
    [Refactor] Change some comments.
    
    [Refactor] Remove useless file.

--- java/api/src/main/java/org/ray/api/runtime/DefaultRayRuntimeFactory.java
+++ java/api/src/main/java/org/ray/api/runtime/DefaultRayRuntimeFactory.java
@@ -2,18 +2,29 @@ package org.ray.api.runtime;
 
 import java.lang.reflect.Method;
 
+import org.ray.api.config.RayConfig;
+
 /**
  * The default Ray runtime factory. It produces an instance of AbstractRayRuntime.
  */
 public class DefaultRayRuntimeFactory implements RayRuntimeFactory {
 
   @Override
-  public RayRuntime createRayRuntime() {
+  public RayRuntime createRayRuntime(RayConfig initConfig) {
     try {
-      Method m = Class.forName("org.ray.runtime.AbstractRayRuntime").getDeclaredMethod("init");
-      m.setAccessible(true);
-      RayRuntime runtime = (RayRuntime) m.invoke(null);
-      m.setAccessible(false);
+      // TODO(qwang): How about use factory to create runtime object?
+      Class clz;
+      if (initConfig.getParams().run_mode.isNativeRuntime()) {
+        clz = Class.forName("org.ray.runtime.RayNativeRuntime");
+      } else {
+        clz = Class.forName("org.ray.runtime.RayDevRuntime");
+      }
+      RayRuntime runtime = (RayRuntime) clz.newInstance();
+
+      Method init = clz.getMethod("init", RayConfig.class);
+      init.setAccessible(true);
+      init.invoke(runtime, initConfig);
+      init.setAccessible(false);
       return runtime;
     } catch (Exception e) {
       throw new RuntimeException("Failed to initialize ray runtime", e);

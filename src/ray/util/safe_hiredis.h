f2ea38936c66b48891e6ee98a72716d206ab4562
qicosmos <qicosmos@163.com>
2020-08-19 14:20:18 +0800

    1.improve and fix; 2.add more test case; 3.add some safe api;

--- src/ray/util/safe_hiredis.h
+++ src/ray/util/safe_hiredis.h
@@ -5,79 +5,110 @@
 #ifndef RAY_SAFE_HIREDIS_H
 #define RAY_SAFE_HIREDIS_H
 
+#include <memory>
 #include "hiredis/hiredis.h"
-namespace ray{
+
+namespace ray {
 class RedisContext final {
  public:
   RedisContext() = delete;
-  RedisContext(const RedisContext&) = delete;
-  RedisContext&operator=(const RedisContext&) = delete;
+  RedisContext(const RedisContext &) = delete;
+  RedisContext &operator=(const RedisContext &) = delete;
 
-  RedisContext(RedisContext&&) = default;
-  RedisContext&operator=(RedisContext&&) = default;
+  RedisContext(RedisContext &&) = default;
+  RedisContext &operator=(RedisContext &&) = default;
   RedisContext(redisContext *context) : ctx_(context) {}
 
-  operator  bool (){
-    return ctx_!= nullptr;
-  }
+  operator bool() { return ctx_ != nullptr && !ctx_->err; }
 
-  redisContext *operator()(){
-    return get();
-  }
+  redisContext *operator()() { return get(); }
 
-  redisContext * get(){
+  redisContext *get() {
+    check();
     return ctx_.get();
   }
 
-  bool operator==(const RedisContext& other) const{
-    return other.ctx_ == this->ctx_;
-  }
+  bool operator==(const RedisContext &other) const { return other.ctx_ == this->ctx_; }
 
-  bool operator!=(const RedisContext& other) const{
-    return other.ctx_ != this->ctx_;
-  }
+  bool operator!=(const RedisContext &other) const { return other.ctx_ != this->ctx_; }
 
-  redisContext *operator->() const { return ctx_.get(); }
+  redisContext *operator->() const {
+    check();
+    return ctx_.get();
+  }
 
  private:
+  void check() const {
+    if (ctx_ == nullptr) {
+      throw std::invalid_argument("redisContext is null");
+    }
+    if (ctx_->err) {
+      throw std::invalid_argument(ctx_->errstr);
+    }
+  }
   std::unique_ptr<redisContext> ctx_;
 };
 
 class RedisReply final {
  public:
   RedisReply() = delete;
-  RedisReply(const RedisReply&) = delete;
-  RedisReply&operator=(const RedisReply&) = delete;
+  RedisReply(const RedisReply &) = delete;
+  RedisReply &operator=(const RedisReply &) = delete;
 
-  RedisReply(RedisReply&&) = default;
-  RedisReply&operator=(RedisReply&&) = default;
+  RedisReply(RedisReply &&) = default;
+  RedisReply &operator=(RedisReply &&) = default;
   RedisReply(redisReply *reply) : reply_(reply) {}
-  RedisReply(void *reply) : reply_(static_cast<redisReply*>(reply)) {}
+  RedisReply(nullptr_t reply) : reply_(nullptr) {}
 
-  operator  bool (){
-    return reply_!= nullptr;
+  RedisReply(void *reply) {
+    if (reply == nullptr) {
+      reply_ = nullptr;
+    } else {
+      reply_ = std::unique_ptr<redisReply>(static_cast<redisReply *>(reply));
+    }
   }
 
-  bool operator==(const RedisReply& other) const{
-    return other.reply_ == this->reply_;
-  }
+  operator bool() { return reply_ != nullptr && reply_->type != REDIS_REPLY_ERROR; }
 
-  bool operator!=(const RedisReply& other) const{
-    return other.reply_ != this->reply_;
-  }
+  bool operator==(const RedisReply &other) const { return other.reply_ == this->reply_; }
 
-  redisReply *operator()(){
-    return get();
-  }
+  bool operator!=(const RedisReply &other) const { return other.reply_ != this->reply_; }
 
-  redisReply * get(){
+  redisReply *operator()() { return get(); }
+
+  redisReply *get() {
+    check();
     return reply_.get();
   }
 
-  redisReply *operator->() const { return reply_.get(); }
+  redisReply *operator->() const {
+    check();
+    return reply_.get();
+  }
 
  private:
+  void check() const {
+    if (reply_ == nullptr) {
+      throw std::invalid_argument("redisContext is null");
+    }
+    if (reply_->type == REDIS_REPLY_ERROR) {
+      throw std::invalid_argument("REDIS_REPLY_ERROR");
+    }
+  }
   std::unique_ptr<redisReply> reply_;
 };
+
+inline RedisContext redisConnect_s(const char *ip, int port) {
+  return redisConnect(ip, port);
+}
+
+inline RedisReply redisCommand_s(redisContext *c, const char *format, ...) {
+  va_list ap;
+  va_start(ap, format);
+  void *reply = redisvCommand(c, format, ap);
+  va_end(ap);
+  return reply;
+}
+
 }
 #endif  // RAY_SAFE_HIREDIS_H
--- src/ray/util/safe_hiredis_test.cc
+++ src/ray/util/safe_hiredis_test.cc
@@ -1,23 +1,79 @@
 //
 // Created by qicosmos on 2020/8/18.
 //
-#include "gtest/gtest.h"
 #include "ray/util/safe_hiredis.h"
 
+#include "gtest/gtest.h"
+
 namespace ray {
-//test passed in local env, make sure the redis server started!
+
+TEST(SafeHiredis, NoNeedCheckNull) {
+  int invalid_port = 0;
+  RedisContext context = redisConnect("127.0.0.1", invalid_port);
+  EXPECT_THROW(context(), std::exception);
+  EXPECT_THROW(context.get(), std::exception);
+  RedisReply auth_reply((void *)nullptr);
+  EXPECT_THROW(auth_reply(), std::exception);
+  EXPECT_THROW(auth_reply.get(), std::exception);
+
+  redisContext* ctx = nullptr;
+  RedisContext c(ctx);
+  EXPECT_FALSE(c);
+  EXPECT_EQ(c, nullptr);
+}
+
+TEST(SafeHiredis, CheckAndCompare) {
+  int invalid_port = 0;
+  RedisContext context1 = redisConnect("127.0.0.1", invalid_port);
+  RedisContext context2 = redisConnect("127.0.0.1", invalid_port);
+  EXPECT_FALSE(context1);
+  EXPECT_FALSE(context2);
+
+  RedisReply auth_reply(nullptr);
+  EXPECT_FALSE(auth_reply);
+
+  redisReply* p = nullptr;
+  RedisReply reply(p);
+  EXPECT_FALSE(auth_reply);
+
+
+  EXPECT_NE(context1, nullptr);
+}
+
 TEST(SafeHiredis, SafeConnect) {
   RedisContext context = redisConnect("127.0.0.1", 6379);
+  if(!context){
+    return;
+  }
   EXPECT_TRUE(context && !context->err);
 }
 
 TEST(SafeHiredis, SafeCommand) {
   RedisContext context = redisConnect("127.0.0.1", 6379);
+  if(!context){
+    return;
+  }
   EXPECT_TRUE(context && !context->err);
   RedisReply auth_reply = (redisReply *)redisCommand(context(), "AUTH %s", "5241590000000000");
   EXPECT_TRUE(auth_reply->type != REDIS_REPLY_ERROR);
 
   RedisReply reply = redisCommand(context(), "GET session_dir");
+  EXPECT_TRUE(reply->type != REDIS_REPLY_ERROR);
+  std::string session_dir(reply->str);
+  EXPECT_TRUE(!session_dir.empty());
+}
+
+TEST(SafeHiredis, SafeAPI){
+  RedisContext context = redisConnect_s("127.0.0.1", 6379);
+  if(!context){
+    return;
+  }
+
+  EXPECT_TRUE(context && !context->err);
+  RedisReply auth_reply = redisCommand_s(context(), "AUTH %s", "5241590000000000");
+  EXPECT_TRUE(auth_reply->type != REDIS_REPLY_ERROR);
+
+  RedisReply reply = redisCommand_s(context(), "GET session_dir");
   EXPECT_TRUE(reply->type != REDIS_REPLY_ERROR);
   std::string session_dir(reply->str);
   EXPECT_TRUE(!session_dir.empty());

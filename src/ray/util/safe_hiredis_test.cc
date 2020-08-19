02b41bea6706f6ab4e6ac4a0be61ccf0278a6ae4
qicosmos <qicosmos@163.com>
2020-08-19 14:31:56 +0800

    add an example code, safe_hiredis make the code shorter and safer!

--- src/ray/util/safe_hiredis_test.cc
+++ src/ray/util/safe_hiredis_test.cc
@@ -7,6 +7,41 @@
 
 namespace ray {
 
+// static std::string GetSessionDir(std::string redis_ip, int port, std::string password)
+// {
+//  redisContext *context = redisConnect(redis_ip.c_str(), port);
+//  RAY_CHECK(context != NULL && !context->err);
+//  if (!password.empty()) {
+//    auto auth_reply = (redisReply *)redisCommand(context, "AUTH %s", password.c_str());
+//    RAY_CHECK(auth_reply->type != REDIS_REPLY_ERROR);
+//    freeReplyObject(auth_reply);
+//  }
+//  auto reply = (redisReply *)redisCommand(context, "GET session_dir");
+//  RAY_CHECK(reply->type != REDIS_REPLY_ERROR);
+//  auto session_dir = std::string(reply->str);
+//  freeReplyObject(reply);
+//  redisFree(context);
+//  return session_dir;
+//}
+
+// VS
+
+//static std::string GetSessionDir(std::string redis_ip, int port, std::string password) {
+//  RedisContext context = redisConnect_s(redis_ip.c_str(), port);
+//
+//  if (!password.empty()) {
+//    RedisReply auth_reply = redisCommand_s(context.get(), "AUTH %s", password.c_str());
+//    EXPECT_TRUE(auth_reply->type != REDIS_REPLY_ERROR);
+//  }
+//
+//  RedisReply reply = redisCommand_s(context(), "GET session_dir");
+//  auto session_dir = std::string(reply->str);
+//
+//  return session_dir;
+//}
+
+//new safe api make the code shorter and safer!
+
 TEST(SafeHiredis, NoNeedCheckNull) {
   int invalid_port = 0;
   RedisContext context = redisConnect("127.0.0.1", invalid_port);
@@ -16,7 +51,7 @@ TEST(SafeHiredis, NoNeedCheckNull) {
   EXPECT_THROW(auth_reply(), std::exception);
   EXPECT_THROW(auth_reply.get(), std::exception);
 
-  redisContext* ctx = nullptr;
+  redisContext *ctx = nullptr;
   RedisContext c(ctx);
   EXPECT_FALSE(c);
   EXPECT_EQ(c, nullptr);
@@ -32,17 +67,16 @@ TEST(SafeHiredis, CheckAndCompare) {
   RedisReply auth_reply(nullptr);
   EXPECT_FALSE(auth_reply);
 
-  redisReply* p = nullptr;
+  redisReply *p = nullptr;
   RedisReply reply(p);
   EXPECT_FALSE(auth_reply);
-
 
   EXPECT_NE(context1, nullptr);
 }
 
 TEST(SafeHiredis, SafeConnect) {
   RedisContext context = redisConnect("127.0.0.1", 6379);
-  if(!context){
+  if (!context) {
     return;
   }
   EXPECT_TRUE(context && !context->err);
@@ -50,11 +84,12 @@ TEST(SafeHiredis, SafeConnect) {
 
 TEST(SafeHiredis, SafeCommand) {
   RedisContext context = redisConnect("127.0.0.1", 6379);
-  if(!context){
+  if (!context) {
     return;
   }
   EXPECT_TRUE(context && !context->err);
-  RedisReply auth_reply = (redisReply *)redisCommand(context(), "AUTH %s", "5241590000000000");
+  RedisReply auth_reply =
+      (redisReply *)redisCommand(context(), "AUTH %s", "5241590000000000");
   EXPECT_TRUE(auth_reply->type != REDIS_REPLY_ERROR);
 
   RedisReply reply = redisCommand(context(), "GET session_dir");
@@ -63,9 +98,9 @@ TEST(SafeHiredis, SafeCommand) {
   EXPECT_TRUE(!session_dir.empty());
 }
 
-TEST(SafeHiredis, SafeAPI){
+TEST(SafeHiredis, SafeAPI) {
   RedisContext context = redisConnect_s("127.0.0.1", 6379);
-  if(!context){
+  if (!context) {
     return;
   }
 

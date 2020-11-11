ffac7f611d6d3eee5d47f770c59215833b3779e4
君熠 <qiaoshuai.qs@antfin.com>
2020-11-11 18:15:11 +0800

    1.reformat code style 2.refactor the code

--- deploy/ray-operator/controllers/raycluster_controller.go
+++ deploy/ray-operator/controllers/raycluster_controller.go
@@ -1,47 +1,47 @@
 package controllers
 
 import (
-	"context"
-	"fmt"
-	mapset "github.com/deckarep/golang-set"
-	"github.com/go-logr/logr"
+    "context"
+    "fmt"
+    mapset "github.com/deckarep/golang-set"
+    "github.com/go-logr/logr"
     "github.com/pborman/uuid"
     _ "k8s.io/api/apps/v1beta1"
-	rayiov1alpha1 "ray-operator/api/v1alpha1"
-	"ray-operator/controllers/common"
-	_ "ray-operator/controllers/common"
-	"ray-operator/controllers/utils"
-	"strings"
+    rayiov1alpha1 "ray-operator/api/v1alpha1"
+    "ray-operator/controllers/common"
+    _ "ray-operator/controllers/common"
+    "ray-operator/controllers/utils"
+    "strings"
 
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	apierrs "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-	"sigs.k8s.io/controller-runtime/pkg/handler"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
-	"sigs.k8s.io/controller-runtime/pkg/source"
+    corev1 "k8s.io/api/core/v1"
+    "k8s.io/apimachinery/pkg/api/errors"
+    apierrs "k8s.io/apimachinery/pkg/api/errors"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/apimachinery/pkg/runtime"
+    ctrl "sigs.k8s.io/controller-runtime"
+    "sigs.k8s.io/controller-runtime/pkg/client"
+    "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+    "sigs.k8s.io/controller-runtime/pkg/handler"
+    "sigs.k8s.io/controller-runtime/pkg/manager"
+    "sigs.k8s.io/controller-runtime/pkg/reconcile"
+    logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+    "sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 var log = logf.Log.WithName("RayCluster-Controller")
 
 // newReconciler returns a new reconcile.Reconciler
 func newReconciler(mgr manager.Manager) reconcile.Reconciler {
-	return &RayClusterReconciler{Client: mgr.GetClient(), Scheme: mgr.GetScheme()}
+    return &RayClusterReconciler{Client: mgr.GetClient(), Scheme: mgr.GetScheme()}
 }
 
 var _ reconcile.Reconciler = &RayClusterReconciler{}
 
 // RayClusterReconciler reconciles a RayCluster object
 type RayClusterReconciler struct {
-	client.Client
-	Log    logr.Logger
-	Scheme *runtime.Scheme
+    client.Client
+    Log    logr.Logger
+    Scheme *runtime.Scheme
 }
 
 // Reconcile reads that state of the cluster for a RayCluster object and makes changes based on it
@@ -55,144 +55,144 @@ type RayClusterReconciler struct {
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 
 func (r *RayClusterReconciler) Reconcile(request reconcile.Request) (reconcile.Result, error) {
-	_ = r.Log.WithValues("raycluster", request.NamespacedName)
-	log.Info("Reconciling RayCluster", "cluster name", request.Name)
+    _ = r.Log.WithValues("raycluster", request.NamespacedName)
+    log.Info("Reconciling RayCluster", "cluster name", request.Name)
 
-	// Fetch the RayCluster instance
-	instance := &rayiov1alpha1.RayCluster{}
-	err := r.Get(context.TODO(), request.NamespacedName, instance)
+    // Fetch the RayCluster instance
+    instance := &rayiov1alpha1.RayCluster{}
+    err := r.Get(context.TODO(), request.NamespacedName, instance)
 
-	if err != nil {
-		if errors.IsNotFound(err) {
-			// Object not found, return.  Created objects are automatically garbage collected.
-			// For additional cleanup logic use finalizers.
-			return reconcile.Result{}, nil
-		}
-		log.Error(err, "Read request instance error!")
-		// Error reading the object - requeue the request.
-		if !apierrs.IsNotFound(err) {
-			return reconcile.Result{}, err
-		}
-		return reconcile.Result{}, nil
-	}
+    if err != nil {
+        if errors.IsNotFound(err) {
+            // Object not found, return.  Created objects are automatically garbage collected.
+            // For additional cleanup logic use finalizers.
+            return reconcile.Result{}, nil
+        }
+        log.Error(err, "Read request instance error!")
+        // Error reading the object - requeue the request.
+        if !apierrs.IsNotFound(err) {
+            return reconcile.Result{}, err
+        }
+        return reconcile.Result{}, nil
+    }
 
-	log.Info("Print instance - ", "Instance.ToString", instance)
+    log.Info("Print instance - ", "Instance.ToString", instance)
 
-	// Build pods for instance
-	expectedPods := r.buildPods(instance)
+    // Build pods for instance
+    expectedPods := r.buildPods(instance)
 
-	expectedPodNameList := mapset.NewSet()
-	expectedPodMap := make(map[string]corev1.Pod)
-	needServicePodMap := mapset.NewSet()
-	for _, pod := range expectedPods {
-		expectedPodNameList.Add(pod.Name)
-		expectedPodMap[pod.Name] = pod
-		if strings.EqualFold(pod.Labels[common.ClusterPodType], common.Head) {
-			needServicePodMap.Add(pod.Name)
-		}
-	}
+    expectedPodNameList := mapset.NewSet()
+    expectedPodMap := make(map[string]corev1.Pod)
+    needServicePodMap := mapset.NewSet()
+    for _, pod := range expectedPods {
+        expectedPodNameList.Add(pod.Name)
+        expectedPodMap[pod.Name] = pod
+        if strings.EqualFold(pod.Labels[common.ClusterPodType], common.Head) {
+            needServicePodMap.Add(pod.Name)
+        }
+    }
 
-	log.Info("Build pods according to the ray cluster instance", "size", len(expectedPods), "podNames", expectedPodNameList)
+    log.Info("Build pods according to the ray cluster instance", "size", len(expectedPods), "podNames", expectedPodNameList)
 
-	runtimePods := corev1.PodList{}
-	if err = r.List(context.TODO(), &runtimePods, client.InNamespace(instance.Namespace), client.MatchingLabels{common.RayClusterOwnerKey: request.Name}); err != nil {
-		return reconcile.Result{}, err
-	}
+    runtimePods := corev1.PodList{}
+    if err = r.List(context.TODO(), &runtimePods, client.InNamespace(instance.Namespace), client.MatchingLabels{common.RayClusterOwnerKey: request.Name}); err != nil {
+        return reconcile.Result{}, err
+    }
 
-	runtimePodNameList := mapset.NewSet()
-	runtimePodMap := make(map[string]corev1.Pod)
-	for _, runtimePod := range runtimePods.Items {
-		runtimePodNameList.Add(runtimePod.Name)
-		runtimePodMap[runtimePod.Name] = runtimePod
-	}
+    runtimePodNameList := mapset.NewSet()
+    runtimePodMap := make(map[string]corev1.Pod)
+    for _, runtimePod := range runtimePods.Items {
+        runtimePodNameList.Add(runtimePod.Name)
+        runtimePodMap[runtimePod.Name] = runtimePod
+    }
 
-	log.Info("Runtime Pods", "size", len(runtimePods.Items), "runtime pods namelist", runtimePodNameList)
+    log.Info("Runtime Pods", "size", len(runtimePods.Items), "runtime pods namelist", runtimePodNameList)
 
-	// Record that the pod needs to be deleted.
-	difference := runtimePodNameList.Difference(expectedPodNameList)
+    // Record that the pod needs to be deleted.
+    difference := runtimePodNameList.Difference(expectedPodNameList)
 
-	// fill replicas with runtime if exists or expectedPod if not exists
-	var replicas []corev1.Pod
-	for _, pod := range expectedPods {
-		if runtimePodNameList.Contains(pod.Name) {
-			replicas = append(replicas, runtimePodMap[pod.Name])
-		} else {
-			replicas = append(replicas, pod)
-		}
-	}
+    // fill replicas with runtime if exists or expectedPod if not exists
+    var replicas []corev1.Pod
+    for _, pod := range expectedPods {
+        if runtimePodNameList.Contains(pod.Name) {
+            replicas = append(replicas, runtimePodMap[pod.Name])
+        } else {
+            replicas = append(replicas, pod)
+        }
+    }
 
-	// Create the head node service.
-	if needServicePodMap.Cardinality() > 0 {
-		for elem := range needServicePodMap.Iterator().C {
-			podName := elem.(string)
-			svcConf := common.DefaultServiceConfig(*instance, podName)
-			rayPodSvc := common.ServiceForPod(svcConf)
-			blockOwnerDeletion := true
-			ownerReference := metav1.OwnerReference{
-				APIVersion:         instance.APIVersion,
-				Kind:               instance.Kind,
-				Name:               instance.Name,
-				UID:                instance.UID,
-				BlockOwnerDeletion: &blockOwnerDeletion,
-			}
-			rayPodSvc.OwnerReferences = append(rayPodSvc.OwnerReferences, ownerReference)
-			if errSvc := r.Create(context.TODO(), rayPodSvc); errSvc != nil {
-				if errors.IsAlreadyExists(errSvc) {
-					log.Info("Pod service already exist,no need to create")
-				} else {
-					log.Error(errSvc, "Pod Service create error!", "Pod.Service.Error", errSvc)
-					return reconcile.Result{}, errSvc
-				}
-			} else {
-				log.Info("Pod Service create anew successfully", "podName", rayPodSvc.Name)
-			}
-		}
-	}
+    // Create the head node service.
+    if needServicePodMap.Cardinality() > 0 {
+        for elem := range needServicePodMap.Iterator().C {
+            podName := elem.(string)
+            svcConf := common.DefaultServiceConfig(*instance, podName)
+            rayPodSvc := common.ServiceForPod(svcConf)
+            blockOwnerDeletion := true
+            ownerReference := metav1.OwnerReference{
+                APIVersion:         instance.APIVersion,
+                Kind:               instance.Kind,
+                Name:               instance.Name,
+                UID:                instance.UID,
+                BlockOwnerDeletion: &blockOwnerDeletion,
+            }
+            rayPodSvc.OwnerReferences = append(rayPodSvc.OwnerReferences, ownerReference)
+            if errSvc := r.Create(context.TODO(), rayPodSvc); errSvc != nil {
+                if errors.IsAlreadyExists(errSvc) {
+                    log.Info("Pod service already exist,no need to create")
+                } else {
+                    log.Error(errSvc, "Pod Service create error!", "Pod.Service.Error", errSvc)
+                    return reconcile.Result{}, errSvc
+                }
+            } else {
+                log.Info("Pod Service create anew successfully", "podName", rayPodSvc.Name)
+            }
+        }
+    }
 
-	// Check if each pod exists and if not, create it.
-	for i, replica := range replicas {
-		if !utils.IsCreated(&replica) {
-			log.Info("Creating pod", "index", i, "create pod", replica.Name)
-			if err := r.Create(context.TODO(), &replica); err != nil {
-				return reconcile.Result{}, err
-			}
-		}
-	}
+    // Check if each pod exists and if not, create it.
+    for i, replica := range replicas {
+        if !utils.IsCreated(&replica) {
+            log.Info("Creating pod", "index", i, "create pod", replica.Name)
+            if err := r.Create(context.TODO(), &replica); err != nil {
+                return reconcile.Result{}, err
+            }
+        }
+    }
 
-	// Delete pods if needed.
-	if difference.Cardinality() > 0 {
-		log.Info("difference", "pods", difference)
-		for _, runtimePod := range runtimePods.Items {
-			if difference.Contains(runtimePod.Name) {
-				log.Info("Deleting pod", "namespace", runtimePod.Namespace, "name", runtimePod.Name)
-				if err := r.Delete(context.TODO(), &runtimePod); err != nil {
-					return reconcile.Result{}, err
-				}
-				if strings.EqualFold(runtimePod.Labels[common.ClusterPodType], common.Head) {
-					svcConf := common.DefaultServiceConfig(*instance, runtimePod.Name)
-					raySvcHead := common.ServiceForPod(svcConf)
-					log.Info("delete head service", "headName", runtimePod.Name)
-					if err := r.Delete(context.TODO(), raySvcHead); err != nil {
-						return reconcile.Result{}, err
-					}
-				}
-			}
-		}
-	}
+    // Delete pods if needed.
+    if difference.Cardinality() > 0 {
+        log.Info("difference", "pods", difference)
+        for _, runtimePod := range runtimePods.Items {
+            if difference.Contains(runtimePod.Name) {
+                log.Info("Deleting pod", "namespace", runtimePod.Namespace, "name", runtimePod.Name)
+                if err := r.Delete(context.TODO(), &runtimePod); err != nil {
+                    return reconcile.Result{}, err
+                }
+                if strings.EqualFold(runtimePod.Labels[common.ClusterPodType], common.Head) {
+                    svcConf := common.DefaultServiceConfig(*instance, runtimePod.Name)
+                    raySvcHead := common.ServiceForPod(svcConf)
+                    log.Info("delete head service", "headName", runtimePod.Name)
+                    if err := r.Delete(context.TODO(), raySvcHead); err != nil {
+                        return reconcile.Result{}, err
+                    }
+                }
+            }
+        }
+    }
 
-	// update cluster when idList generated by controller
+    // update cluster when idList generated by controller
     _ = r.Update(context.Background(), instance)
 
-	return reconcile.Result{}, nil
+    return reconcile.Result{}, nil
 }
 
 // Build cluster instance pods.
 func (r *RayClusterReconciler) buildPods(instance *rayiov1alpha1.RayCluster) []corev1.Pod {
-	var pods []corev1.Pod
-	var extensions []rayiov1alpha1.Extension
-	if instance.Spec.Extensions != nil && len(instance.Spec.Extensions) > 0 {
-		for _, extension := range instance.Spec.Extensions {
-            if extension.IdList != nil && len(extension.IdList) >0 {
+    var pods []corev1.Pod
+    var extensions []rayiov1alpha1.Extension
+    if instance.Spec.Extensions != nil && len(instance.Spec.Extensions) > 0 {
+        for _, extension := range instance.Spec.Extensions {
+            if extension.IdList != nil && len(extension.IdList) > 0 {
                 for _, podName := range extension.IdList {
                     podType := fmt.Sprintf("%v", extension.Type)
                     pods = r.createAndBindPod(instance, podType, podName, extension, pods)
@@ -201,12 +201,7 @@ func (r *RayClusterReconciler) buildPods(instance *rayiov1alpha1.RayCluster) []c
                 var i int32 = 0
                 for i = 0; i < *extension.Replicas; i++ {
                     podType := fmt.Sprintf("%v", extension.Type)
-                    podName := ""
-                    if extension.GroupName == "" {
-                        podName = instance.Name + common.DashSymbol + extension.GroupName + common.DashSymbol + podType + common.DashSymbol + uuid.New()[0:6]
-                    } else {
-                        podName = instance.Name + common.DashSymbol + podType + common.DashSymbol + uuid.New()[0:6]
-                    }
+                    podName := generatePodName(instance, extension, podType)
                     pods = r.createAndBindPod(instance, podType, podName, extension, pods)
                     extension.IdList = append(extension.IdList, podName)
                 }
@@ -223,6 +218,16 @@ func (r *RayClusterReconciler) buildPods(instance *rayiov1alpha1.RayCluster) []c
     return pods
 }
 
+func generatePodName(instance *rayiov1alpha1.RayCluster, extension rayiov1alpha1.Extension, podType string) string {
+    podName := ""
+    if extension.GroupName == "" {
+        podName = instance.Name + common.DashSymbol + extension.GroupName + common.DashSymbol + podType + common.DashSymbol + uuid.New()[0:6]
+    } else {
+        podName = instance.Name + common.DashSymbol + podType + common.DashSymbol + uuid.New()[0:6]
+    }
+    return podName
+}
+
 func (r *RayClusterReconciler) createAndBindPod(instance *rayiov1alpha1.RayCluster, podType string, podName string, extension rayiov1alpha1.Extension, pods []corev1.Pod) []corev1.Pod {
     podConf := common.DefaultPodConfig(instance, podType, podName)
     podConf.Extension = extension
@@ -237,11 +242,11 @@ func (r *RayClusterReconciler) createAndBindPod(instance *rayiov1alpha1.RayClust
 
 // SetupWithManager builds the reconciler.
 func (r *RayClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&rayiov1alpha1.RayCluster{}).
-		Watches(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
-			IsController: true,
-			OwnerType:    &rayiov1alpha1.RayCluster{},
-		}).
-		Complete(r)
+    return ctrl.NewControllerManagedBy(mgr).
+        For(&rayiov1alpha1.RayCluster{}).
+        Watches(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
+            IsController: true,
+            OwnerType:    &rayiov1alpha1.RayCluster{},
+        }).
+        Complete(r)
 }

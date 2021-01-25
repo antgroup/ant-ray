803e108c5b1cd05258c25fd87076ee23fcd80e6f
Qing Wang <jovany.wq@antgroup.com>
2021-01-25 14:28:07 +0800

    TRY

--- java/test/src/main/java/io/ray/test/AnnotationTransformer.java
+++ java/test/src/main/java/io/ray/test/AnnotationTransformer.java
@@ -9,7 +9,8 @@ public class AnnotationTransformer implements IAnnotationTransformer {
 
   @Override
   public void transform(
-      ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
-    annotation.setRetryAnalyzer(RetryAnalyzer.class);
-  }
+      ITestAnnotation annotation,
+      Class testClass,
+      Constructor testConstructor,
+      Method testMethod) {}
 }

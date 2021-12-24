f942afaa5e279cb203191474bcd93ce761a5cb0b
mubai <chaokun.yck@antfin.com>
2021-12-24 13:40:26 +0800

    remove serialization code

--- java/runtime/src/main/java/io/ray/runtime/serialization/serializers/SerializerFactory.java
+++
@@ -1,8 +0,0 @@
-package io.ray.runtime.serialization.serializers;
-
-import io.ray.runtime.serialization.Fury;
-
-public interface SerializerFactory {
-
-  Serializer createSerializer(Fury fury, Class<?> cls);
-}
--- java/runtime/src/main/java/io/ray/runtime/serialization/util/CommonUtil.java
+++
@@ -1,5 +0,0 @@
-package io.ray.runtime.serialization.util;
-
-public class CommonUtil {
-  public static void ignore(Object... args) {}
-}

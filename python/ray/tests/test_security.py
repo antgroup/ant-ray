8d785d1c33d08c31cd61e8ea8cef068f9f8749fc
久龙 <guyang.sgy@antfin.com>
2022-05-24 17:02:56 +0800

    add test

--- python/ray/tests/test_security.py
+++ python/ray/tests/test_security.py
@@ -49,5 +49,34 @@ def test_remote_function_whitelist(shutdown_only, enable_whitelist, valid_whitel
             ray.get(foo.remote())
 
 
+@pytest.fixture
+def disable_remote_code():
+    os.environ["RAY_DISABLE_REMOTE_CODE"] = "true"
+    yield
+    del os.environ["RAY_DISABLE_REMOTE_CODE"]
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="Fails on windows")
+def test_load_code_mode(disable_remote_code, shutdown_only):
+    ray.init()
+
+    @ray.remote
+    def f():
+        return 1
+
+    with pytest.raises(ray.exceptions.WorkerCrashedError):
+        ray.get(f.remote())
+
+    @ray.remote
+    class Foo:
+        def foo(self):
+            return "OK"
+
+    foo_actor = Foo.remote()
+    # TODO(SongGuyang): Throw FunctionLoadingError for actor tasks.
+    with pytest.raises(ray.exceptions.RayActorError):
+        ray.get(foo_actor.foo.remote())
+
+
 if __name__ == "__main__":
     sys.exit(pytest.main(["-v", __file__]))

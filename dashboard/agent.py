5cd1c7d385d0620b9b7631cc2fb38eb2ee22f7bc
JasonChen <892670992@qq.com>
2022-11-10 10:59:54 +0800

    Revert "disable agent parent check"
    This reverts commit e8eea6903635ea83407af4af1e50dbca1365b5e0.

--- dashboard/agent.py
+++ dashboard/agent.py
@@ -156,8 +156,8 @@ class DashboardAgent(object):
                 logger.error("Failed to check parent PID, exiting.")
                 sys.exit(1)
 
-        # if sys.platform not in ["win32", "cygwin"]:
-        #     check_parent_task = create_task(_check_parent())
+        if sys.platform not in ["win32", "cygwin"]:
+            check_parent_task = create_task(_check_parent())
 
         # Start a grpc asyncio server.
         if self.server:
@@ -208,8 +208,8 @@ class DashboardAgent(object):
         )
 
         tasks = [m.run(self.server) for m in modules]
-        # if sys.platform not in ["win32", "cygwin"]:
-        #     tasks.append(check_parent_task)
+        if sys.platform not in ["win32", "cygwin"]:
+            tasks.append(check_parent_task)
         await asyncio.gather(*tasks)
 
         await self.server.wait_for_termination()

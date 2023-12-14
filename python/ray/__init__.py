77843485cb045270246ee9aa2fc06c63b95dc305
Jason Chen <892670992@qq.com>
2023-12-15 00:24:26 +0800

    post2 version with minor fix (#405)
    Signed-off-by: JasonChen <892670992@qq.com>

--- python/ray/__init__.py
+++ python/ray/__init__.py
@@ -110,7 +110,7 @@ del _configure_system
 
 # Replaced with the current commit when building the wheels.
 __commit__ = "{{RAY_COMMIT_SHA}}"
-__version__ = "1.13.0post1"
+__version__ = "1.13.0post2"
 
 import ray._raylet  # noqa: E402
 

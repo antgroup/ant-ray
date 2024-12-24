cc4d7958a880e727d091a72c5f08e38a497fff3a
sule <sule.sl@antgroup.com>
2024-12-24 15:34:13 +0800

    Add logs for error cases
    Signed-off-by: sule <sule.sl@antgroup.com>

--- src/ray/gcs/gcs_server/gcs_virtual_cluster_manager.cc
+++ src/ray/gcs/gcs_server/gcs_virtual_cluster_manager.cc
@@ -130,14 +130,21 @@ void GcsVirtualClusterManager::HandleCreateJobCluster(
                 << virtual_cluster_id;
   auto virtual_cluster = GetVirtualCluster(virtual_cluster_id);
   if (virtual_cluster == nullptr) {
-    GCS_RPC_SEND_REPLY(
-        send_reply_callback, reply, Status::NotFound("virtual cluster not exists"));
+    std::ostringstream ostr;
+    ostr << "Create job cluster for job " << request.job_id()
+         << " failed, virtual cluster not exists: " << virtual_cluster_id;
+    std::string message = ostr.str();
+    RAY_LOG(ERROR) << message;
+    GCS_RPC_SEND_REPLY(send_reply_callback, reply, Status::NotFound(message));
     return;
   }
   if (virtual_cluster->GetMode() != rpc::AllocationMode::EXCLUSIVE) {
-    GCS_RPC_SEND_REPLY(send_reply_callback,
-                       reply,
-                       Status::InvalidArgument("virtual cluster is not exclusive"));
+    std::ostringstream ostr;
+    ostr << "Create job cluster for job " << request.job_id()
+         << " failed, virtual cluster is not exclusive: " << virtual_cluster_id;
+    std::string message = ostr.str();
+    RAY_LOG(ERROR) << message;
+    GCS_RPC_SEND_REPLY(send_reply_callback, reply, Status::InvalidArgument(message));
     return;
   }
   ReplicaSets replica_sets(request.replica_sets().begin(), request.replica_sets().end());
@@ -149,12 +156,14 @@ void GcsVirtualClusterManager::HandleCreateJobCluster(
   exclusive_cluster->CreateJobCluster(
       job_cluster_id,
       std::move(replica_sets),
-      [reply, send_reply_callback](const Status &status,
-                                   std::shared_ptr<rpc::VirtualClusterTableData> data) {
+      [reply, send_reply_callback, job_id = request.job_id()](
+          const Status &status, std::shared_ptr<rpc::VirtualClusterTableData> data) {
         if (status.ok()) {
           reply->set_job_cluster_id(data->id());
           GCS_RPC_SEND_REPLY(send_reply_callback, reply, Status::OK());
         } else {
+          RAY_LOG(ERROR) << "Create job cluster for job " << job_id << " failed, "
+                         << status.message();
           GCS_RPC_SEND_REPLY(send_reply_callback, reply, status);
         }
       });

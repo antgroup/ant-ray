b2d6f507c9e97fe00d281c2a2ac5575c08b74ab4
Chong Li <lc300133@antgroup.com>
2025-04-01 15:27:36 +0800

    Temp test
    Signed-off-by: Chong Li <lc300133@antgroup.com>

--- python/ray/_version.py
+++ python/ray/_version.py
@@ -1,6 +1,6 @@
 # Replaced with the current commit when building the wheels.
 commit = "{{RAY_COMMIT_SHA}}"
-version = "3.0.0.dev1"
+version = "2.44.1.post0"
 
 if __name__ == "__main__":
     print("%s %s" % (version, commit))

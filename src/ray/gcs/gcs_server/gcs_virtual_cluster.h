1406d464e471ceeb16887f5a778dcdefa8bc438d
Chong Li <lc300133@antgroup.com>
2025-04-28 23:15:06 +0800

    Fix remove nodes from virtual cluster
    Signed-off-by: Chong Li <lc300133@antgroup.com>

--- src/ray/gcs/gcs_server/gcs_virtual_cluster.h
+++ src/ray/gcs/gcs_server/gcs_virtual_cluster.h
@@ -258,7 +258,13 @@ class VirtualCluster {
   std::shared_ptr<NodeInstance> ReplenishUndividedNodeInstance(
       std::shared_ptr<NodeInstance> node_instance_to_replenish);
 
-  Status RemoveNodeInstances(const std::vector<std::string> &nodes_to_remove);
+  /// Remove specified nodes from a virtual cluster.
+  ///
+  /// \param nodes_to_remove The nodes to be removed.
+  /// \param removed_replica_instances The replica instances that actually removed based
+  /// on the `nodes_to_remove`. \return The status of the removal.
+  Status RemoveNodeInstances(const std::vector<std::string> &nodes_to_remove,
+                             ReplicaInstances *removed_replica_instances);
 
  protected:
   /// Insert the node instances to the cluster.

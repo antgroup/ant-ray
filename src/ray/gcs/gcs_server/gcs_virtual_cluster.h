cab37ce4957b2435ee7089b301e45969f8b70b50
Chong Li <lc300133@antgroup.com>
2025-04-29 20:12:38 +0800

    Add param[out]
    Signed-off-by: Chong Li <lc300133@antgroup.com>

--- src/ray/gcs/gcs_server/gcs_virtual_cluster.h
+++ src/ray/gcs/gcs_server/gcs_virtual_cluster.h
@@ -260,8 +260,8 @@ class VirtualCluster {
 
   /// Remove specified nodes from a virtual cluster.
   ///
-  /// \param nodes_to_remove The nodes to be removed.
-  /// \param removed_replica_instances The replica instances that were
+  /// \param[in] nodes_to_remove The nodes to be removed.
+  /// \param[out] removed_replica_instances The replica instances that were
   /// actually removed based on the `nodes_to_remove`.
   /// \return The status of the removal.
   Status RemoveNodeInstances(const std::vector<std::string> &nodes_to_remove,

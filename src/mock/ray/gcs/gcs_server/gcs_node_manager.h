107193005858e0659c28a6a6c725acec347f5737
BaoMi <ww15897971129@gmail.com>
2025-07-04 10:52:31 +0800

    modify gcs_node_manager.h

--- src/mock/ray/gcs/gcs_server/gcs_node_manager.h
+++ src/mock/ray/gcs/gcs_server/gcs_node_manager.h
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#pragma once
 #include "gmock/gmock.h"
 #include "ray/gcs/gcs_server/gcs_node_manager.h"
 #include "ray/gcs/gcs_server/gcs_virtual_cluster_manager.h"

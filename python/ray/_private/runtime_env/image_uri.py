814e9d89d27430e5c39afd0aebba028bc44a4c90
xianying.ly <xianying.ly@antgroup.com>
2025-11-26 20:07:39 +0800

    Add test for output filter

--- python/ray/_private/runtime_env/image_uri.py
+++ python/ray/_private/runtime_env/image_uri.py
@@ -50,14 +50,15 @@ async def _create_impl(image_uri: str, logger: logging.Logger):
 
     output_filter = os.getenv("RAY_PODMAN_OUTPUT_FILTER", "")
     if output_filter:
-        safe_worker_path = shlex.quote(worker_path)
-        safe_output_filter = shlex.quote(output_filter)
-        filter_cmd = ["sh", "-c", f"printf '%s' {safe_worker_path} | {safe_output_filter}"]
-        filtered_path = await check_output_cmd(filter_cmd, logger=logger)
-        worker_path = filtered_path
-
+        worker_path = await _apply_output_filter(logger, worker_path, output_filter)
     return worker_path.strip()
 
+async def _apply_output_filter(logger, worker_path, output_filter):
+    safe_worker_path = shlex.quote(worker_path)
+    filter_cmd = ["sh", "-c", f"printf '%s' {safe_worker_path} | {output_filter}"]
+    filtered_path = await check_output_cmd(filter_cmd, logger=logger)
+    worker_path = filtered_path
+    return worker_path
 
 def _modify_container_context_impl(
     runtime_env: "RuntimeEnv",  # noqa: F821
@@ -173,9 +174,9 @@ def _modify_container_context_impl(
 
     redirected_pyenv_folder = None
     if container_install_ray or container_pip_packages:
-        container_to_host_mount_dict[
-            container_dependencies_installer_path
-        ] = get_dependencies_installer_path()
+        container_to_host_mount_dict[container_dependencies_installer_path] = (
+            get_dependencies_installer_path()
+        )
         if runtime_env_constants.RAY_PODMAN_UES_WHL_PACKAGE:
             container_to_host_mount_dict[get_ray_whl_dir()] = get_ray_whl_dir()
 

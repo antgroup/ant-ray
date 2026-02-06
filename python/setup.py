32cb02e27da45b43f653c7780de533c90f69ec6f
will <zzchun8@gmail.com>
2026-02-06 18:47:05 +0800

    bump version to z1

--- python/setup.py
+++ python/setup.py
@@ -94,7 +94,7 @@ class SetupSpec:
         elif build_type == BuildType.NIGHTLY:
             version_postfix = datetime.today().strftime("%Y%m%d")
             version = re.sub(r"dev\d*", f"dev{version_postfix}", version)
-            self.version: str = version + ".b4"
+            self.version: str = version + ".z1"
             self.name = f"{self.name}-nightly"
         elif build_type == BuildType.DEPS_ONLY:
             self.version: str = DEPS_ONLY_VERSION
